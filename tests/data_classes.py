8c2a97dbf48dbd5f6f426a0c9e1748235c593d91
Quentin Thébault <quentin.thebault@defenso.fr>
2024-09-05 10:07:19 +0900

    revert useless non-proofing in data_classes.py

--- tests/data_classes.py
+++ tests/data_classes.py
@@ -462,9 +462,7 @@ class Jail(Resource):
             release = '-'.join(self.release.rsplit('-')[:2])
 
         template = '-'
-        jail_origin = self.root_dataset[
-            'properties'
-        ].get('origin', {}).get('value')
+        jail_origin = self.root_dataset['properties']['origin']['value']
         if jail_origin:
             template = jail_origin.rsplit(
                 '/root@', 1
