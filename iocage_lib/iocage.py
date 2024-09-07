7186e7f6675fc05ab702ee0baca942ff69aed7e5
Quentin Thébault <quentin.thebault@defenso.fr>
2024-09-07 18:11:39 +0900

    remove fetch debug print

--- iocage_lib/iocage.py
+++ iocage_lib/iocage.py
@@ -1008,8 +1008,6 @@ class IOCage:
 
         freebsd_version = ioc_common.checkoutput(["freebsd-version"])
         arch = os.uname()[4]
-        
-        print("Fetch kwargs:", kwargs)
 
         if not _list:
             if not kwargs.get('files', None):
