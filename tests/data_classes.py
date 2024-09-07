8f1106696118cc978be33c3ba89ed5208a043bad
Quentin Thébault <quentin.thebault@defenso.fr>
2024-09-07 19:47:52 +0900

    Remove None from cloned_snapshots

--- tests/data_classes.py
+++ tests/data_classes.py
@@ -825,6 +825,7 @@ class ResourceSelector:
             jail.jail_dataset['properties']['origin']['value']
             for jail in cloned_jails
         }
+        origins -= { None }
         return {
             Snapshot(origin, origin.rsplit('@', 1)[0])
             for origin in origins
