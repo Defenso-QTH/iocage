cda1b3aa6966087be06f71f5238cf843830e2e5a
Quentin Thébault <quentin.thebault@defenso.fr>
2024-09-23 16:56:38 +0900

    update 0020_remove_snapshot_test.py - unfold the right one

--- tests/functional_tests/0020_remove_snapshot_test.py
+++ tests/functional_tests/0020_remove_snapshot_test.py
@@ -154,9 +154,7 @@ def test_04_remove_all_snapshots_success(invoke_cli, resource_selector,
     )
 
     assert all(snap.exists is False for snap in filtered_remove_snaps)
-    #assert all(snap.exists is True for snap in cloned_snaps)
-    for snap in cloned_snaps:
-        assert snap.exists is True
+    assert all(snap.exists is True for snap in cloned_snaps)
 
 
 @require_root
@@ -188,4 +186,6 @@ def test_05_remove_all_snapshots_all_jails(invoke_cli, resource_selector,
     )
 
     assert all(snap.exists is False for snap in filtered_remove_snaps)
-    assert all(snap.exists is True for snap in cloned_snaps)
+    #assert all(snap.exists is True for snap in cloned_snaps)
+    for snap in cloned_snaps:
+            assert snap.exists is True
