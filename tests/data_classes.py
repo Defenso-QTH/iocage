ff1edb232266522b3655dc9a7f42dce45500f571
Quentin Thébault <quentin.thebault@defenso.fr>
2024-09-26 12:36:16 +0900

    update data_classes.py - use sysrc pool and prefix

--- tests/data_classes.py
+++ tests/data_classes.py
@@ -284,6 +284,13 @@ class ZFS:
     def set_pool(self):
         if not self.pool:
             with libzfs.ZFS() as zfs:
+                sysrc_pool = get_sysrc('zfs_pool')
+                if sysrc_pool is not None:
+                    ZFS.pool = sysrc_pool
+                    ZFS.pool_mountpoint =
+                        zfs.get(ZFS.pool).root_dataset.mountpoint
+                    ZFS.prefix = get_sysrc('zfs_prefix') or ''
+                    return
                 pools = [
                     p for p in zfs.pools
                     if p.root_dataset.__getstate__().get(
